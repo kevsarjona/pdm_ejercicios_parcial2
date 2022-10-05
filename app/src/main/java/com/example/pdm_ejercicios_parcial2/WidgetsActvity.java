f389f8916bf7ce4201e26aee3a11d78d6568d4aa
kevsarjona <kevinlovelp@gmail.com>
2022-10-04 20:58:39 -0500

    cambios widgets 04 oct

--- app/src/main/java/com/example/pdm_ejercicios_parcial2/WidgetsActvity.java
+++ app/src/main/java/com/example/pdm_ejercicios_parcial2/WidgetsActvity.java
@@ -7,15 +7,30 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class WidgetsActvity extends AppCompatActivity {
 
     Button myButton;
     SeekBar seekBar;
     TextView textView;
+    EditText editText;
+    CheckBox checkBox;
+    Switch aSwitch;
+    RadioGroup radioGroup;
+    RatingBar ratingBar;
+    ProgressBar progressBar;
+    ToggleButton toggleButton;
 
 
 
@@ -27,9 +42,22 @@ public class WidgetsActvity extends AppCompatActivity {
         myButton = findViewById(R.id.btn_buton);
         seekBar = findViewById(R.id.seek_bar);
         textView = findViewById(R.id.text_view);
+        editText = findViewById(R.id.edit_text);
+        checkBox = findViewById(R.id.checkbox);
+        aSwitch = findViewById(R.id.swtch);
+        radioGroup = findViewById(R.id.radio_group);
+        ratingBar = findViewById(R.id.rating_bar);
+        progressBar = findViewById(R.id.progress_bar);
+        toggleButton = findViewById(R.id.toggle_button);
 
         listenerButton();
         listenerTextView();
+        listenerEditText();
+        listenerCheckbox();
+        listenerSwitch();
+        listenerRadioGroup();
+        listenerRatingBar();
+        listenerToggleButton();
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -81,6 +109,11 @@ public class WidgetsActvity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 message("changedTextView" + charSequence);
+                /*String mayusculas = charSequence.toString().toUpperCase();
+                String currentValue = textView.getText().toString().toUpperCase();
+                if(!mayusculas != currentValue) {
+                    textView.setText(mayusculas);
+                }*/
 
             }
 
@@ -88,6 +121,85 @@ public class WidgetsActvity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
                 message("afterTextView");
 
+
+            }
+        });
+    }
+
+    private void listenerEditText(){
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                message("BeforeTextChanged" +  charSequence + " " + i + " " + i1 + " " + i2 );
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                message("onTextChanged" +  charSequence + " " + i + " " + i1 + " " + i2);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                message("afterTextChanged");
+            }
+        });
+    }
+
+    private void listenerCheckbox(){
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                message("onCheckedChanged " + b);
+
+            }
+        });
+    }
+
+    private void listenerSwitch() {
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                message("onCheckedChanged " + b);
+            }
+        });
+    }
+
+    private void listenerRadioGroup(){
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.option1:
+                        message("opcion 1");
+                        break;
+
+                    case R.id.option2:
+                        message("opcion 2");
+                        break;
+                }
+
+            }
+        });
+    }
+
+    private void listenerRatingBar(){
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                message("onRatingChanged " +  v + " " + b);
+            }
+        });
+    }
+
+    private void listenerProgressBar(){
+
+    }
+
+    private void listenerToggleButton(){
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                message("onCheckedChanged " + b);
             }
         });
     }
