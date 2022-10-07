121ac52bfd435c898cd664e9274a7ec873292f96
kevsarjona <kevinlovelp@gmail.com>
2022-10-06 21:46:19 -0500

    Advanced Widgets

--- app/src/main/java/com/example/pdm_ejercicios_parcial2/AdvancedWidgetsActivity.java
+++ app/src/main/java/com/example/pdm_ejercicios_parcial2/AdvancedWidgetsActivity.java
@@ -1,24 +1,83 @@
 package com.example.pdm_ejercicios_parcial2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class AdvancedWidgetsActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_widgets);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
         setTitle("Mi titulo");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_favorite:
+                        message("Pulsaste favorito");
+                        return true;
+                    case R.id.menu_copiar:
+                        message("Pulsaste copiar");
+                        return true;
+                    case R.id.menu_remove:
+                        message("Pulsaste Eliminar");
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_favorite:
+                message("Pulsaste favorito");
+                break;
+            case R.id.menu_copiar:
+                message("Pulsaste copiar");
+                break;
+            case R.id.menu_remove:
+                message("Pulsaste Eliminar");
+                break;
+            case android.R.id.home:
+                message("Pulsaste Home");
+                break;
+
+            default:
+                message("Pulsaste otra opcion");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void message(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
 
--- app/src/main/java/com/example/pdm_ejercicios_parcial2/MainActivity.java
+++ app/src/main/java/com/example/pdm_ejercicios_parcial2/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button btnWidgets;
+    Button btnAdvancedWidgets;
 
 
     @Override
@@ -19,10 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnWidgets = findViewById(R.id.btn_elements);
+        btnAdvancedWidgets = findViewById(R.id.btn_advanced_elements);
+
         btnWidgets.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, WidgetsActvity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAdvancedWidgets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AdvancedWidgetsActivity.class);
                 startActivity(intent);
             }
         });
